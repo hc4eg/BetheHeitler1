fd9dc39ea29e921f8c943b4bfc7831e2880e5e60
haoyu <hc4eg@virginia.edu>
2016-01-11 16:44:03 -0500

    1. Create TpTeSym*,C files.    They are used to:    1) Randomly generate e- or e+ with random energy, direction    2) Compute the other one's energy, direction    3) Plot energy, angle distribution of both particle, check symmetry.
    2. Usage:
       1) Open root
       2) Type ".L TpTeSym.C++" , if check 3D case type ".L TpTeSym3D.C++"
       3) Type "TpTeSym()" or "TpTeSym3D()"
       And check result.

---
+++ RootScipt/TpTeSym_C.d
@@ -0,0 +1,44 @@
+
+# DO NOT DELETE
+
+./TpTeSym_C.so: /usr/include/root/TFile.h /usr/include/root/TDirectoryFile.h
+./TpTeSym_C.so: /usr/include/root/TDirectory.h /usr/include/root/TNamed.h
+./TpTeSym_C.so: /usr/include/root/TObject.h /usr/include/root/Rtypes.h
+./TpTeSym_C.so: /usr/include/root/RConfig.h /usr/include/root/RVersion.h
+./TpTeSym_C.so: /usr/include/root/DllImport.h /usr/include/root/Rtypeinfo.h
+./TpTeSym_C.so: /usr/include/root/snprintf.h /usr/include/root/strlcpy.h
+./TpTeSym_C.so: /usr/include/root/TGenericClassInfo.h
+./TpTeSym_C.so: /usr/include/root/TSchemaHelper.h
+./TpTeSym_C.so: /usr/include/root/TStorage.h
+./TpTeSym_C.so: /usr/include/root/TVersionCheck.h /usr/include/root/Riosfwd.h
+./TpTeSym_C.so: /usr/include/root/TBuffer.h /usr/include/root/TString.h
+./TpTeSym_C.so: /usr/include/root/TMathBase.h /usr/include/root/TList.h
+./TpTeSym_C.so: /usr/include/root/TSeqCollection.h
+./TpTeSym_C.so: /usr/include/root/TCollection.h /usr/include/root/TIterator.h
+./TpTeSym_C.so: /usr/include/root/TDatime.h /usr/include/root/TUUID.h
+./TpTeSym_C.so: /usr/include/root/TMap.h /usr/include/root/THashTable.h
+./TpTeSym_C.so: /usr/include/root/TUrl.h /usr/include/root/TTree.h
+./TpTeSym_C.so: /usr/include/root/TBranch.h /usr/include/root/TObjArray.h
+./TpTeSym_C.so: /usr/include/root/TAttFill.h /usr/include/root/TDataType.h
+./TpTeSym_C.so: /usr/include/root/TDictionary.h /usr/include/root/Property.h
+./TpTeSym_C.so: /usr/include/root/ESTLType.h /usr/include/root/TAttLine.h
+./TpTeSym_C.so: /usr/include/root/TAttMarker.h /usr/include/root/TArrayD.h
+./TpTeSym_C.so: /usr/include/root/TArray.h /usr/include/root/TArrayI.h
+./TpTeSym_C.so: /usr/include/root/TClass.h /usr/include/root/TObjString.h
+./TpTeSym_C.so: /usr/include/root/TVirtualTreePlayer.h
+./TpTeSym_C.so: /usr/include/root/TRandom.h /usr/include/root/TCanvas.h
+./TpTeSym_C.so: /usr/include/root/TPad.h /usr/include/root/TVirtualPad.h
+./TpTeSym_C.so: /usr/include/root/TAttPad.h /usr/include/root/TVirtualX.h
+./TpTeSym_C.so: /usr/include/root/TAttText.h /usr/include/root/GuiTypes.h
+./TpTeSym_C.so: /usr/include/root/Buttons.h /usr/include/root/TQObject.h
+./TpTeSym_C.so: /usr/include/root/TAttCanvas.h /usr/include/root/TCanvasImp.h
+./TpTeSym_C.so: /usr/include/root/TH1.h /usr/include/root/TAxis.h
+./TpTeSym_C.so: /usr/include/root/TAttAxis.h /usr/include/root/TArrayC.h
+./TpTeSym_C.so: /usr/include/root/TArrayS.h /usr/include/root/TArrayF.h
+./TpTeSym_C.so: /usr/include/root/Foption.h /usr/include/root/TVectorFfwd.h
+./TpTeSym_C.so: /usr/include/root/TVectorDfwd.h
+./TpTeSym_C.so: /usr/include/root/TFitResultPtr.h /usr/include/root/TH2.h
+./TpTeSym_C.so: /usr/include/root/TMatrixFBasefwd.h
+./TpTeSym_C.so: /usr/include/root/TMatrixDBasefwd.h
+./TpTeSym_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+TpTeSym_C__ROOTBUILDVERSION= 5.34/14
