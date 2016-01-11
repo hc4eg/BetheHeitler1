e63ca5ada5092065a469532a3b1292d0432e73d1
haoyu <hc4eg@virginia.edu>
2016-01-11 16:31:03 -0500

    1. Create Ee-Ep*.C files    Usage is: extract data from root file (with new abbreviate naming)    , plot following histograms:    1)Ee-Ep    2)Ee+Ep    3)Opening Angle    4)Opening Angle vs Delta (Energy Difference)    5)Asymmetry vs Delta    6)Asymmetry vs Delta near Opening Angle 10 Degree
    2. Usage:
       1)open root
       2)Type ".L Ee-Ep.C++"
       3)Type "Histo()"
       4)Type root file name

---
+++ RootScipt/Ee-EpTest_C.d
@@ -0,0 +1,50 @@
+
+# DO NOT DELETE
+
+./Ee-EpTest_C.so: /usr/include/root/TFile.h
+./Ee-EpTest_C.so: /usr/include/root/TDirectoryFile.h
+./Ee-EpTest_C.so: /usr/include/root/TDirectory.h /usr/include/root/TNamed.h
+./Ee-EpTest_C.so: /usr/include/root/TObject.h /usr/include/root/Rtypes.h
+./Ee-EpTest_C.so: /usr/include/root/RConfig.h /usr/include/root/RVersion.h
+./Ee-EpTest_C.so: /usr/include/root/DllImport.h /usr/include/root/Rtypeinfo.h
+./Ee-EpTest_C.so: /usr/include/root/snprintf.h /usr/include/root/strlcpy.h
+./Ee-EpTest_C.so: /usr/include/root/TGenericClassInfo.h
+./Ee-EpTest_C.so: /usr/include/root/TSchemaHelper.h
+./Ee-EpTest_C.so: /usr/include/root/TStorage.h
+./Ee-EpTest_C.so: /usr/include/root/TVersionCheck.h
+./Ee-EpTest_C.so: /usr/include/root/Riosfwd.h /usr/include/root/TBuffer.h
+./Ee-EpTest_C.so: /usr/include/root/TString.h /usr/include/root/TMathBase.h
+./Ee-EpTest_C.so: /usr/include/root/TList.h
+./Ee-EpTest_C.so: /usr/include/root/TSeqCollection.h
+./Ee-EpTest_C.so: /usr/include/root/TCollection.h
+./Ee-EpTest_C.so: /usr/include/root/TIterator.h /usr/include/root/TDatime.h
+./Ee-EpTest_C.so: /usr/include/root/TUUID.h /usr/include/root/TMap.h
+./Ee-EpTest_C.so: /usr/include/root/THashTable.h /usr/include/root/TUrl.h
+./Ee-EpTest_C.so: /usr/include/root/TTree.h /usr/include/root/TBranch.h
+./Ee-EpTest_C.so: /usr/include/root/TObjArray.h /usr/include/root/TAttFill.h
+./Ee-EpTest_C.so: /usr/include/root/TDataType.h
+./Ee-EpTest_C.so: /usr/include/root/TDictionary.h
+./Ee-EpTest_C.so: /usr/include/root/Property.h /usr/include/root/ESTLType.h
+./Ee-EpTest_C.so: /usr/include/root/TAttLine.h /usr/include/root/TAttMarker.h
+./Ee-EpTest_C.so: /usr/include/root/TArrayD.h /usr/include/root/TArray.h
+./Ee-EpTest_C.so: /usr/include/root/TArrayI.h /usr/include/root/TClass.h
+./Ee-EpTest_C.so: /usr/include/root/TObjString.h
+./Ee-EpTest_C.so: /usr/include/root/TVirtualTreePlayer.h
+./Ee-EpTest_C.so: /usr/include/root/TClonesArray.h
+./Ee-EpTest_C.so: /usr/include/root/Riostream.h /usr/include/root/TCanvas.h
+./Ee-EpTest_C.so: /usr/include/root/TPad.h /usr/include/root/TVirtualPad.h
+./Ee-EpTest_C.so: /usr/include/root/TAttPad.h /usr/include/root/TVirtualX.h
+./Ee-EpTest_C.so: /usr/include/root/TAttText.h /usr/include/root/GuiTypes.h
+./Ee-EpTest_C.so: /usr/include/root/Buttons.h /usr/include/root/TQObject.h
+./Ee-EpTest_C.so: /usr/include/root/TAttCanvas.h
+./Ee-EpTest_C.so: /usr/include/root/TCanvasImp.h /usr/include/root/TGraph.h
+./Ee-EpTest_C.so: /usr/include/root/TVectorFfwd.h
+./Ee-EpTest_C.so: /usr/include/root/TVectorDfwd.h
+./Ee-EpTest_C.so: /usr/include/root/TFitResultPtr.h /usr/include/root/TH1.h
+./Ee-EpTest_C.so: /usr/include/root/TAxis.h /usr/include/root/TAttAxis.h
+./Ee-EpTest_C.so: /usr/include/root/TArrayC.h /usr/include/root/TArrayS.h
+./Ee-EpTest_C.so: /usr/include/root/TArrayF.h /usr/include/root/Foption.h
+./Ee-EpTest_C.so: /usr/include/root/TH2.h /usr/include/root/TMatrixFBasefwd.h
+./Ee-EpTest_C.so: /usr/include/root/TMatrixDBasefwd.h
+./Ee-EpTest_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+Ee-EpTest_C__ROOTBUILDVERSION= 5.34/14
