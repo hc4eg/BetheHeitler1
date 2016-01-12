53870aa324f3b320f0c81f37e05a2f23cfa721cf
haoyu <hc4eg@virginia.edu>
2016-01-11 22:11:00 -0500

    1. Create files Plot*.C    They are used for:    1)Read .root simulation files with "Or" condition (If one of 4 detectors has hit, then write data)    2)Apply cut to the root file plot all data in "And" condition (Only all 4 detectors have hit, write data)    3)PlotNew.C is write to make multiple histogram in one TCanvas.
    2. Usage:
       1)Open root.
       2)Type ".L Plot.C++" Or ".L PlotNew.C++"
       3)Type "PlotMonitor()", "PlotVDC()", "PlotPaddle()" to check the histogram of corresponding detector.
       4)Type name of the root file to analyze.

---
+++ RootScipt/PlotNew.C
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "TFile.h"
+#include "TTree.h"
+#include "TObject.h" 
+#include "TClonesArray.h"
+#include "Riostream.h"
+#include "TCanvas.h"
+#include <cmath>
+
+using namespace std;
+
+//Usage of Plot.C:
+// Ananlyzing simulation root files, extract data by using AND cut (Only take events with all 4 detector hit)
+
+
+void PlotMonitor(){
+  //Open the root file
+  string filename;
+  cout<< "Input datafile name: (Format: ****.root)" << endl;
+  getline(cin,filename);
+  cout << endl;
+  TFile * histofile = TFile::Open(filename.c_str());
+
+  TTree* T = new TTree();
+  T = (TTree * ) histofile->Get("T");
+
+  TString Cut("B.M1.Charge !=0 && B.D0.V.E0u > 0.0002 && B.D0.V.E0v > 0.0002 && B.D0.V.E1u > 0.0002 && B.D0.V.E1v > 0.0002 && B.D0.P.Edep > 0.02 && B.M0.Charge !=0 && B.D1.V.E0u > 0.0002 && B.D1.V.E0v > 0.0002 && B.D1.V.E1u > 0.0002 && B.D1.V.E1v > 0.0002 && B.D1.P.Edep > 0.02");
+    
+    TCanvas* CENum = new TCanvas("CEnum", "ENum",800,400);
+
+    TCanvas* CM0 = new TCanvas ("CM0", "Monitor0 data", 1600, 1500);
+    CM0->Divide(3,2);
+
+    TCanvas* CM1 = new TCanvas ("CM1", "Monitor1 data", 1600, 1500);
+    CM1->Divide(3,2);
+
+    CENum->cd();           T->Draw("B.ENum",Cut);
+    
+    CM1->cd(1);            T->Draw("B.M1.Charge",Cut);
+    CM1->cd(2);            T->Draw("B.M1.Energy",Cut);
+    CM1->cd(3);            T->Draw("B.M1.X",Cut);
+    CM1->cd(4);            T->Draw("B.M1.Y",Cut);
+    CM1->cd(5);            T->Draw("B.M1.Theta",Cut);
+    CM1->cd(6);            T->Draw("B.M1.Phi",Cut);
+
+    CM0->cd(1);            T->Draw("B.M0.Charge",Cut);
+    CM0->cd(2);            T->Draw("B.M0.Energy",Cut);
+    CM0->cd(3);            T->Draw("B.M0.X",Cut);
+    CM0->cd(4);            T->Draw("B.M0.Y",Cut);
+    CM0->cd(5);            T->Draw("B.M0.Theta",Cut);
+    CM0->cd(6);            T->Draw("B.M0.Phi",Cut);
+}
+
+void PlotVDC(){
+  //Open the root file
+  string filename;
+  cout<< "Input datafile name: (Format: ****.root)" << endl;
+  getline(cin,filename);
+  cout << endl;
+  TFile * histofile = TFile::Open(filename.c_str());
+
+  TTree* T = new TTree();
+  T = (TTree * ) histofile->Get("T");
+
+  TString Cut("B.M1.Charge !=0 && B.D0.V.E0u > 0.0002 && B.D0.V.E0v > 0.0002 && B.D0.V.E1u > 0.0002 && B.D0.V.E1v > 0.0002 && B.D0.P.Edep > 0.02 && B.M0.Charge !=0 && B.D1.V.E0u > 0.0002 && B.D1.V.E0v > 0.0002 && B.D1.V.E1u > 0.0002 && B.D1.V.E1v > 0.0002 && B.D1.P.Edep > 0.02");
+
+
+    TCanvas* CENum = new TCanvas("CEnum", "ENum",800,400);
+
+    TCanvas* CD1V = new TCanvas ("D1VDC", "Detector1 VDC data", 1600, 1000);
+    CD1V->Divide(2,2);
+
+    CENum->cd();             T->Draw("B.ENum",Cut);
+    
+    CD1V->cd(1);             T->Draw("B.D1.V.X",Cut);
+    CD1V->cd(2);             T->Draw("B.D1.V.Y",Cut);
+    CD1V->cd(3);             T->Draw("B.D1.V.Theta",Cut);
+    CD1V->cd(4);             T->Draw("B.D1.V.Phi",Cut);
+
+    TCanvas* CD0V = new TCanvas ("D0VDC", "Detector0 VDC data", 1600, 1000);
+    CD0V->Divide(2,2);    
+
+    CD0V->cd(1);             T->Draw("B.D0.V.X",Cut);
+    CD0V->cd(2);             T->Draw("B.D0.V.Y",Cut);
+    CD0V->cd(3);             T->Draw("B.D0.V.Theta",Cut);
+    CD0V->cd(4);             T->Draw("B.D0.V.Phi",Cut);
+}
+
+void PlotPaddle(){
+  //Open the root file
+  string filename;
+  cout<< "Input datafile name: (Format: ****.root)" << endl;
+  getline(cin,filename);
+  cout << endl;
+  TFile * histofile = TFile::Open(filename.c_str());
+
+  TTree* T = new TTree();
+  T = (TTree * ) histofile->Get("T");
+
+  TString Cut("B.M1.Charge !=0 && B.D0.V.E0u > 0.0002 && B.D0.V.E0v > 0.0002 && B.D0.V.E1u > 0.0002 && B.D0.V.E1v > 0.0002 && B.D0.P.Edep > 0.02 && B.M0.Charge !=0 && B.D1.V.E0u > 0.0002 && B.D1.V.E0v > 0.0002 && B.D1.V.E1u > 0.0002 && B.D1.V.E1v > 0.0002 && B.D1.P.Edep > 0.02");
+
+    TCanvas* CENum = new TCanvas("CEnum", "ENum",800,400);
+
+    TCanvas* CD1P = new TCanvas("D1P","Detector1 Paddle data",1600,1200);
+    CD1P->Divide(2,2);
+
+    CENum->cd();              T->Draw("B.ENum",Cut);
+
+    CD1P->cd(1);              T->Draw("B.D1.P.PNum",Cut);
+    CD1P->cd(2);              T->Draw("B.D1.P.Edep",Cut);
+    CD1P->cd(3);              T->Draw("B.D1.P.Light",Cut);
+    CD1P->cd(4);              T->Draw("B.D1.P.Time",Cut);
+
+    TCanvas* CD0P = new TCanvas("D0P","Detector0 Paddle data",1600,1200);
+    CD0P->Divide(2,2);
+
+    CD0P->cd(1);              T->Draw("B.D0.P.PNum >> (29,0,29)",Cut);
+    CD0P->cd(2);              T->Draw("B.D0.P.Edep",Cut);
+    CD0P->cd(3);              T->Draw("B.D0.P.Light",Cut);
+    CD0P->cd(4);              T->Draw("B.D0.P.Time",Cut);
+}
---
+++ RootScipt/PlotNew_C.d
@@ -0,0 +1,37 @@
+
+# DO NOT DELETE
+
+./PlotNew_C.so: /usr/include/root/TFile.h /usr/include/root/TDirectoryFile.h
+./PlotNew_C.so: /usr/include/root/TDirectory.h /usr/include/root/TNamed.h
+./PlotNew_C.so: /usr/include/root/TObject.h /usr/include/root/Rtypes.h
+./PlotNew_C.so: /usr/include/root/RConfig.h /usr/include/root/RVersion.h
+./PlotNew_C.so: /usr/include/root/DllImport.h /usr/include/root/Rtypeinfo.h
+./PlotNew_C.so: /usr/include/root/snprintf.h /usr/include/root/strlcpy.h
+./PlotNew_C.so: /usr/include/root/TGenericClassInfo.h
+./PlotNew_C.so: /usr/include/root/TSchemaHelper.h
+./PlotNew_C.so: /usr/include/root/TStorage.h
+./PlotNew_C.so: /usr/include/root/TVersionCheck.h /usr/include/root/Riosfwd.h
+./PlotNew_C.so: /usr/include/root/TBuffer.h /usr/include/root/TString.h
+./PlotNew_C.so: /usr/include/root/TMathBase.h /usr/include/root/TList.h
+./PlotNew_C.so: /usr/include/root/TSeqCollection.h
+./PlotNew_C.so: /usr/include/root/TCollection.h /usr/include/root/TIterator.h
+./PlotNew_C.so: /usr/include/root/TDatime.h /usr/include/root/TUUID.h
+./PlotNew_C.so: /usr/include/root/TMap.h /usr/include/root/THashTable.h
+./PlotNew_C.so: /usr/include/root/TUrl.h /usr/include/root/TTree.h
+./PlotNew_C.so: /usr/include/root/TBranch.h /usr/include/root/TObjArray.h
+./PlotNew_C.so: /usr/include/root/TAttFill.h /usr/include/root/TDataType.h
+./PlotNew_C.so: /usr/include/root/TDictionary.h /usr/include/root/Property.h
+./PlotNew_C.so: /usr/include/root/ESTLType.h /usr/include/root/TAttLine.h
+./PlotNew_C.so: /usr/include/root/TAttMarker.h /usr/include/root/TArrayD.h
+./PlotNew_C.so: /usr/include/root/TArray.h /usr/include/root/TArrayI.h
+./PlotNew_C.so: /usr/include/root/TClass.h /usr/include/root/TObjString.h
+./PlotNew_C.so: /usr/include/root/TVirtualTreePlayer.h
+./PlotNew_C.so: /usr/include/root/TClonesArray.h
+./PlotNew_C.so: /usr/include/root/Riostream.h /usr/include/root/TCanvas.h
+./PlotNew_C.so: /usr/include/root/TPad.h /usr/include/root/TVirtualPad.h
+./PlotNew_C.so: /usr/include/root/TAttPad.h /usr/include/root/TVirtualX.h
+./PlotNew_C.so: /usr/include/root/TAttText.h /usr/include/root/GuiTypes.h
+./PlotNew_C.so: /usr/include/root/Buttons.h /usr/include/root/TQObject.h
+./PlotNew_C.so: /usr/include/root/TAttCanvas.h /usr/include/root/TCanvasImp.h
+./PlotNew_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+PlotNew_C__ROOTBUILDVERSION= 5.34/14
